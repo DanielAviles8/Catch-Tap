634eb8f2fb5cb3148860c536ce69a272011de651
Daniel <daniel88tb@gmail.com>
2021-10-01 21:14:43 -0500

    Rotatin-Complete
    Rotación completa con colliders a los lados, falta spawn y colición de proyectiles

---
+++ Assets/Code/Player/PlayerMovement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject Player;
+    public UnityEvent OnPrepare;
+    public bool Active
+    {
+        get{return active;}
+        set{active = value;}
+    }
+
+    Keyboard keyboard;
+    CharacterController characterController;
+
+    private bool active;
+    public Vector3 rotationDirection;
+
+
+    void Prepare()
+    {
+        #if UNITY_STANDALONE_WIN || UNITY_EDITOR
+        keyboard = Keyboard.current;
+        #endif
+        characterController = GetComponent<CharacterController>();
+            active = true;
+            if(OnPrepare != null)
+            {
+                OnPrepare.Invoke();
+            }
+    }
+
+    public void Start()
+    {
+        Prepare();
+    }
+
+    void Update()
+    {
+        if(active)
+        {
+            if(keyboard != null)
+            {
+                CheckInputKeyBoard();
+            }
+        }
+    }
+
+    void CheckInputKeyBoard()
+    {
+        if(keyboard.dKey.wasPressedThisFrame)
+        {
+            Player.transform.rotation = Player.transform.rotation * Quaternion.AngleAxis(90f, rotationDirection);
+            print("derecha");
+        }
+        if(keyboard.aKey.wasPressedThisFrame)
+        {
+            Player.transform.rotation = Player.transform.rotation * Quaternion.AngleAxis(-45f, rotationDirection);
+            print("izquierda");
+        }
+    }
+}
